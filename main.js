c4c964ceeef8b4a31a74c9a89bcaf78918792149
yeon <choiseyeoni@gmail.com>
2021-09-15 13:07:40 +0900

    Add RegExp Options(Flags)

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ https://www.omdbapi.com/?apikey=7035c60c&s=frozen
 The quick brown fox jumps over the lazy dog.
 abbcccdddd
 `
-
+/*
 // const regexp = new RegExp('the', 'gi')
 // const regexp = /the/gi
 // console.log(str.match(regexp))
@@ -19,3 +19,7 @@ const regexp = /fox/gi
 str = str.replace(regexp, 'AAA')
 console.log(str)
 // replace는 원본이 바뀌지 않으므로 원본을 바꿔주고 싶으면 바뀐 데이터를 이런 식으로 재할당해주는 방식을 취하면 됨
+*/
+
+// 정규표현식의 플래그(옵션)들을 사용해서 앞에 있는 패턴을 좀 더 풍부하게 활용할 수 있다.
+console.log(str.match(/\.$/gim))
